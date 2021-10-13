8880948f59ea7f04e9fc276cbed5e2a24589a4f0
arnaldossm <prof.arnaldosilva@gmail.com>
2021-10-13 14:22:12 -0300

    Cria um novo teste

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ const aplicarDesconto = (valor, desconto) => {
   return valor - desconto
 }
 
-module.exports = { aplicarDesconto }
+const debitarSaldo = (saldoAnterior, valor) => {
+  if (saldoAnterior < 0) throw new Error("Saldo negativo") 
+  if (saldoAnterior < valor) throw new Error("Saldo insuficiente") 
+
+    return saldoAnterior - valor
+  
+}
+
+
+
+module.exports = { aplicarDesconto, debitarSaldo }
+
--- index.test.js
+++ index.test.js
@@ -4,3 +4,12 @@ test('Aplicar desconto', () => {
   const result = index.aplicarDesconto(10, 5)
   expect(result).toEqual(5)
 })
+
+
+
+test('debitarSaldo', () => {
+  const result = index.debitarSaldo(10, 5)
+  expect(result).toBeGreaterThanOrEqual(0)
+  expect(result).toEqual(5)
+  
+})
